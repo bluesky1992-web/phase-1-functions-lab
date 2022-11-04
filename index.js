e9e2677c7dd2c0f0daef9de4f2b84c49d1624af5
Ali ibrahim jasim <ali.ibrahim.jasim@gmail.com>
2022-11-05 01:16:07 +0300

    will work on the last function

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 // Code your solution in this file!
 const distanceFromHqInBlocks = (pickupLoc) => {
-  if (pickupLoc > 42) {
-    let res = pickupLoc - 42;
-    return res;
-  } else if (pickupLoc < 42) {
-    let res = pickupLoc - 42;
-    return Math.abs(res); // translate the negative number into a numbers without minus sign
+  if (pickupLoc > 42 || pickupLoc < 42) {
+    const res = pickupLoc - 42;
+    return Math.abs(res);
   }
 };
 console.log(distanceFromHqInBlocks(40));
+
+///
 
 const distanceFromHqInFeet = (inFeet) => {
   const result = distanceFromHqInBlocks(inFeet) * 264;
   return result;
 };
-console.log(distanceFromHqInFeet(40))
+console.log(distanceFromHqInFeet(34));
+
+const distanceTravelledInFeet = (start, end) => {
+  const result = (start - end) * 264;
+  return Math.abs(result);
+};
+console.log(distanceTravelledInFeet(43, 48));
+
+const calculatesFarePrice = (start, destination) => {
+  const Cost = distanceTravelledInFeet(start, destination);
+ 
+};
+console.log(calculatesFarePrice(50, 44));
