034b1f63f08562d365be2d6f2fba0fb33164b63e
Ali ibrahim jasim <ali.ibrahim.jasim@gmail.com>
2022-11-04 17:27:21 +0300

    6 tests done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+const distanceFromHqInBlocks = (pickupLoc) => {
+  if (pickupLoc > 42) {
+    let res = pickupLoc - 42;
+    return res;
+  } else if (pickupLoc < 42) {
+    let res = pickupLoc - 42;
+    return Math.abs(res); // translate the negative number into a numbers without minus sign
+  }
+};
+console.log(distanceFromHqInBlocks(40));
+
+const distanceFromHqInFeet = (inFeet) => {
+  const result = distanceFromHqInBlocks(inFeet) * 264;
+  return result;
+};
+console.log(distanceFromHqInFeet(40))
